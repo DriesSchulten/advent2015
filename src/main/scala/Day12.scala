585e1b2c151c3ed79e40d0fb5073c76cdf91eb11
Dries Schulten <dries.schulten@gmail.com>
2015-12-18 11:28:58 +0100

    Day 12 part 2.

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -4,12 +4,15 @@ import org.json4s.native.JsonMethods._
 object Day12 extends App {
   val input = parse(getClass.getResourceAsStream("Day12.input"))
 
-  def sumNumbers(input: JValue): Int = input match {
+  def sumNumbers(json: JValue, fieldFilter: (JObject => Boolean)): Int = json match {
     case JInt(x) => x.toInt
-    case JArray(arr) => arr.map(sumNumbers).sum
-    case JObject(fields) => fields.map(field => sumNumbers(field._2)).sum
+    case JArray(arr) => arr.map(sumNumbers(_, fieldFilter)).sum
+    case obj @ JObject(fields) if fieldFilter(obj) => fields.map(field => sumNumbers(field._2, fieldFilter)).sum
     case _ => 0
   }
 
-  println(sumNumbers(input))
+  println(sumNumbers(input, _ => true))
+  println(sumNumbers(input, obj => !obj.values.exists {
+    case (_, value) => value == "red"
+  }))
 }
